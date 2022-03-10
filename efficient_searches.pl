fe2dd80eb0caa328a97284da5ba7a34363be3e2c
Maruf Bepary <bepary71@gmail.com>
2022-03-10 18:19:25 +0000

    (T2B) Implement infinite spaces for DFS - Code implemented - Documentation and comments added

--- efficient_searches.pl
+++ efficient_searches.pl
@@ -25,3 +25,30 @@ dfs_helper(Path, Visited, Solution) :-
     append(Path, [Node], NewPath), % Add the node to the path
     dfs_helper(NewPath, [Node|Visited], Solution). % Call dfs_helper with the new path and the new visited list
 
+% Question B
+/**
+ * The function first calls itself with a path that is one step shorter than the current path, and a new empty list. 
+ * If the function is able to find a solution in the new list, it returns the solution. If not, the function recursively calls itself with the updated path and list.
+*/
+dldfs(Path, D, Solution) :-
+    dldfs_helper(Path, D, [], Solution). % Call dldfs_helper with an empty path, depth, and an empty solution
+
+/**
+ * Helper function used to find the goal state for a given path. 
+ * The function takes a path and a number of steps as input, and then returns the goal state for that path.
+*/
+dldfs_helper(Path, 0, _, Path) :-
+    goal(Path). 
+
+/**
+ * It takes a path, a distance, and a list of visited nodes, and finds a solution for the distance remaining. 
+ * If the distance remaining is 0, the function returns the solution. 
+ * If the distance remaining is not 0, the function recurses, taking a new path and a new distance.
+*/
+dldfs_helper(Path, D, Visited, Solution) :-
+    D > 0, % Check if the distance is greater than 0
+    member(Node, Path), % Check if the node is in the path
+    \+ member(Node, Visited), % Check if the node is not in the visited list
+    append(Path, [Node], NewPath), % Add the node to the path
+    NewD is D - 1, % Decrement the distance
+    dldfs_helper(NewPath, NewD, [Node|Visited], Solution). % Call dldfs_helper with the new path and the new distance
